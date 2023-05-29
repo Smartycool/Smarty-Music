f9358a830227c62842b8f9beb7a42e6a63f6714e
YASH <129831825+PRADHAN474@users.noreply.github.com>
2023-05-29 09:44:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,19 +14,19 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "sᴛᴇʟʟᴀ ダ ᴍᴜsɪᴄ")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍𓃭 ⃟ᴍᴜsɪᴄ⛦⃕͜༆")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5616727536").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5059737154").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Devansh-Bots/DevvXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/PRADHAN474/OXY-Music")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/About_Devansh")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Heartless_Talks")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DESISWAGGERHU")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DESI_SWAGGER_NETWORK")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
