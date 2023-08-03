bd8b14767b9fc2408b9e78a6db583e479b7225b7
Smartycool <140168331+Smartycool@users.noreply.github.com>
2023-08-03 18:26:02 +0530

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BWANDARLOK")
-                await self.one.join_chat("BWANDARLOK")
+                await self.one.join_chat("THE_MAFIA_CLAN")
+                await self.one.join_chat("THE_MAFIA_CLAN")
             except:
                 pass
             assistants.append(1)
@@ -79,8 +79,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("BWANDARLOK")
-                await self.two.join_chat("BWANDARLOK")
+                await self.two.join_chat("THE_MAFIA_CLAN")
+                await self.two.join_chat("THE_MAFIA_CLAN")
             except:
                 pass
             assistants.append(2)
@@ -110,8 +110,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BWANDARLOK")
-                await self.three.join_chat("BWANDARLOK")
+                await self.three.join_chat("THE_MAFIA_CLAN")
+                await self.three.join_chat("THE_MAFIA_CLAN")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BWANDARLOK")
-                await self.four.join_chat("BWANDARLOK")
+                await self.four.join_chat("THE_MAFIA_CLAN")
+                await self.four.join_chat("THE_MAFIA_CLAN")
             except:
                 pass
             assistants.append(4)
@@ -172,8 +172,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BWANDARLOK")
-                await self.five.join_chat("BWANDARLOK")
+                await self.five.join_chat("THE_MAFIA_CLAN")
+                await self.five.join_chat("THE_MAFIA_CLAN")
             except:
                 pass
             assistants.append(5)
